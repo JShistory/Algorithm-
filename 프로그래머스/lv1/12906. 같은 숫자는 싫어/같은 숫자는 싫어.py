00dab0ed93acc66271c20fd623c7b693eccd0d21
김재박 <98608723+JShistory@users.noreply.github.com>
2022-08-02 23:55:52 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 119.05 ms, Memory: 27.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,11 @@
+def solution(arr):
+    answer = []
+    answer.append(arr[0])
+    count = 0
+    for i in range(0,len(arr)-1):
+        if arr[i] != arr[i+1]:
+            answer.append(arr[i+1])
+                                        
+            
+        
+    return answer
