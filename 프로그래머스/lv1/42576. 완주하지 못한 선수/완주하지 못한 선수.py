e725854db0bba66437c834c956680967aedb6d8e
김재박 <98608723+JShistory@users.noreply.github.com>
2022-08-02 14:32:11 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 76.04 ms, Memory: 26.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,9 @@
+def solution(participant, completion):
+    answer = ''
+    participant.sort()
+    completion.sort()
+    for p,c in zip(participant,completion):
+        
+        if p!=c:
+            return p
+    return participant.pop()
