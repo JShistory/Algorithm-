2ecabd6973457172ec0d14c36adcd38fc161e169
김재박 <98608723+JShistory@users.noreply.github.com>
2022-08-09 15:09:54 +0900

    [level 3] Title: 베스트앨범, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.py
@@ -1,26 +1,25 @@
 def solution(genres, plays):
-    total={} #장르별 총 재생횟수
-    gen_dic={} #장르별 [재생횟수&고유번호]
-    answer= []
-    for i in range(len(genres)):
-        ng = genres[i]
-        np = plays[i]
-        if ng in total:
-            total[ng] += np
-            gen_dic[ng].append((np,i))
-        else:
-            total[ng] = np
-            gen_dic[ng] = [(np,i)]
+    answer = []
+    g_dict = {}
+    p_dict = {}
     
-    total = sorted(total.items(), key=lambda x : x[1], reverse = True)
-    # print(total)
-    # print(gen_dic)
-    for i in total:
-        playlist = gen_dic[i[0]]
-        # print(playlist)
-        playlist = sorted(playlist, key=lambda x : x[0], reverse = True)
+    for i in range(len(genres)):
+        if genres[i] not in g_dict:
+            g_dict[genres[i]] = plays[i]
+            p_dict[genres[i]] = [(plays[i],i)]
+        else:
+            g_dict[genres[i]] +=plays[i]
+            p_dict[genres[i]] += [(plays[i],i)]
+    g_dict = sorted(g_dict.items(), key=lambda x : x[1], reverse = True)
+    
+
+    for i in g_dict:
+        playlist = p_dict[i[0]]
+        playlist = sorted(playlist, key=lambda x: x[0], reverse = True)
         for j in range(len(playlist)):
-            if j == 2:
+            if j==2:
                 break
             answer.append(playlist[j][1])
+        
+    
     return answer
