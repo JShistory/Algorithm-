53e465e01ccc7cfc6063927c67e2020f77d222a4
김재박 <98608723+JShistory@users.noreply.github.com>
2022-08-10 14:48:45 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -0,0 +1,39 @@
+def solution(dartResult):
+    answer = 0
+    dart = []
+    a = 0
+    count = 0
+    for i in dartResult:
+        if count == 1 and i == '0':
+            a = 10
+        elif ord(i) >= 48 and ord(i) <=57:
+            a = i
+            count = 1
+
+        elif i == 'S':
+            dart.append(int(a)**1)
+            count =0
+        
+        elif i == 'D':
+            dart.append(int(a)**2)
+            count =0
+            
+        elif i == 'T':
+            dart.append(int(a)**3)
+            count =0
+        elif i == '*':
+            if len(dart) < 2:
+                dart[-1] = dart[-1] * 2
+                count =0
+            else:
+                dart[-1] = dart[-1] * 2
+                dart[-2] = dart[-2] * 2
+                count =0
+        
+        elif i == '#':
+            dart[-1] = dart[-1] * -1
+            count =0
+        
+            
+    print(dart)
+    return sum(dart)
