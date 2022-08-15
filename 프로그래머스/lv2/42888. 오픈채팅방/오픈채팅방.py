045cb09d6ef1300cc705139aa98acaedc94c3604
김재박 <98608723+JShistory@users.noreply.github.com>
2022-08-15 15:40:32 +0900

    [level 2] Title: 오픈채팅방, Time: 133.61 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.py
@@ -2,13 +2,13 @@ def solution(record):
     answer = []
     user_ip = {}
     for i in record:
-        if i.split()[0] == 'Enter':
+        if i.split()[0] == "Enter":
             user_ip[i.split()[1]] = i.split()[2]
-        elif i.split()[0] == 'Change':
+        elif i.split()[0] == "Change":
             user_ip[i.split()[1]] = i.split()[2]
     for i in record:
-        if i.split()[0] == 'Enter':
+        if i.split()[0] == "Enter":
             answer.append(user_ip[i.split()[1]]+"님이 들어왔습니다.")
-        elif i.split()[0] == 'Leave':
+        if i.split()[0] == "Leave":
             answer.append(user_ip[i.split()[1]]+"님이 나갔습니다.")
     return answer
