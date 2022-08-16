b257f13e89bebe59222d3456e34bc5a394513841
김재박 <98608723+JShistory@users.noreply.github.com>
2022-08-16 16:35:34 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 0.40 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -0,0 +1,18 @@
+from itertools import combinations
+from collections import Counter
+def solution(orders, course):
+    answer = []
+    cooking = []
+    for i in course:
+        cooking = []
+        for j in orders:
+            for x in combinations(j,i):
+                cook = "".join(sorted(x))
+                cooking.append(cook)
+        
+        sorted_cooking = Counter(cooking).most_common()
+        for menu, cnt in sorted_cooking:
+            if cnt > 1 and cnt == sorted_cooking[0][1]:
+                answer.append(menu)
+                
+    return sorted(answer)
