372c40fd7e0baddc4a3637302737b1e018021d55
김재박 <98608723+JShistory@users.noreply.github.com>
2022-08-16 17:19:11 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.38 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,10 +1,8 @@
-from collections import deque
 def solution(n):
     answer = 0
     if n<3:
         return n
     jump= [0] * (n+1)
-    queue = deque()
     jump[1] = 1
     jump[2] = 2
     for i in range(3,n+1):
