dbd35c9e500d04e1077e9ce6174f211b8e9c1f91
김재박 <98608723+JShistory@users.noreply.github.com>
2022-08-31 21:37:37 +0900

    Update 적록색약.py

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,4 +1,7 @@
+import sys
 from collections import deque
+sys.setrecursionlimit(10000)
+
 n = int(input())
 #일반 사람의 색깔
 data1 = []
@@ -23,33 +26,59 @@ for i in range(n):
             data2[i][j] = "B"
 # print(data)
 # print(color)
-def bfs(graph,x,y,visited):
-    q = deque()
-    q.append((x,y))
-    visited[x][y] = True
-    while q:
-        x,y = q.popleft()
-        for i in range(4):
-            nx = dx[i] + x
-            ny = dy[i] + y
-            if 0<=nx<n and 0<=ny<n:
-                if graph[x][y] == graph[nx][ny] and visited[nx][ny] == False:
-                    q.append((nx,ny))
-                    visited[nx][ny] = True
-    return True
+
+#bfs
+# def bfs(graph,x,y,visited):
+#     q = deque()
+#     q.append((x,y))
+#     visited[x][y] = True
+#     while q:
+#         x,y = q.popleft()
+#         for i in range(4):
+#             nx = dx[i] + x
+#             ny = dy[i] + y
+#             if 0<=nx<n and 0<=ny<n:
+#                 if graph[x][y] == graph[nx][ny] and visited[nx][ny] == False:
+#                     q.append((nx,ny))
+#                     visited[nx][ny] = True
+#     return True
 #일반 사람의 색깔 답
+# answer1 = 0
+# #색맹의 색깔 답
+# answer2 = 0
+# for i in range(n):
+#     for j in range(n):
+#         #만약에 일반 사람의 방문처리가 False라면(방문하지 않았다면)
+#         if not visited1[i][j]:
+#             if bfs(data1, i, j, visited1):
+#                 answer1 +=1
+#         #색맹 사람의 방문처리가 False라면(방문하지 않았다면)
+#         if not visited2[i][j]:
+#             if bfs(data2, i, j, visited2):
+#                 answer2 += 1
+
+#dfs
+def dfs(graph,x,y,visited):
+    visited[x][y] = True
+    cur_color = graph[x][y]
+    for i in range(4):
+        nx = dx[i] + x
+        ny = dy[i] + y
+        if 0<=nx<n and 0<=ny<n:
+            if cur_color == graph[nx][ny] and visited[nx][ny] == False:
+                dfs(graph,nx,ny,visited)
+
 answer1 = 0
-#색맹의 색깔 답
 answer2 = 0
 for i in range(n):
     for j in range(n):
-        #만약에 일반 사람의 방문처리가 False라면(방문하지 않았다면)
         if not visited1[i][j]:
-            if bfs(data1, i, j, visited1):
-                answer1 +=1
-        #색맹 사람의 방문처리가 False라면(방문하지 않았다면)
+            dfs(data1,i,j,visited1)
+            answer1+=1
         if not visited2[i][j]:
-            if bfs(data2, i, j, visited2):
-                answer2 += 1
+            dfs(data2,i,j,visited2)
+            answer2+=1
+
+
 print(answer1, answer2)
-# def dfs(graph, start):
+
