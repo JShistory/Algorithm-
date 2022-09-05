2ebb8b005e23528fed199fd63a51435b2e15a72e
김재박 <98608723+JShistory@users.noreply.github.com>
2022-09-05 21:02:25 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -0,0 +1,11 @@
+from itertools import combinations
+def solution(number, k):
+    answer = []
+    
+    for num in number:
+        while k > 0 and answer and answer[-1] < num:
+            answer.pop()
+            k-=1
+        answer.append(num)
+        
+    return ''.join(answer[:len(answer) - k])
