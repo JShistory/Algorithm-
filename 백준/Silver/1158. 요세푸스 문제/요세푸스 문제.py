de27a0cb785ae3689a87bab5734c0a42d0fe071f
김재박 <98608723+JShistory@users.noreply.github.com>
2022-09-06 17:56:34 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 132 ms, Memory: 114488 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,12 +1,12 @@
-from collections import deque
 n,k = map(int, input().split(" "))
-list = deque()
+list = []
 answer = []
 for i in range(1,n+1):
     list.append(i)
-
-while len(list) != 0:
-    list.rotate(-k)
-    answer.append(list.pop())
-
-print('<' + ', '.join(map(str, answer)) + '>')
+idx = 0
+for i in range(n):
+    idx += k-1
+    if idx >= len(list):
+        idx %=len(list)
+    answer.append(str(list.pop(idx)))
+print("<"+", ".join(answer)+">")
