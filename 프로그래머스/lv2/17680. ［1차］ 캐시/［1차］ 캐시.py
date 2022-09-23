d419cbcbb99e7c9445e12bce79b0a43d10499846
김재박 <98608723+JShistory@users.noreply.github.com>
2022-09-23 11:41:50 +0900

    [level 2] Title: [1차] 캐시, Time: 0.83 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -5,13 +5,16 @@ def solution(cacheSize, cities):
     if cacheSize == 0:
         return 5*len(cities)
     for city in cities:
+        #대소문자 구분 없이
         city = city.lower()
         #만약 캐시가 있으면
         if city in cache:
             answer += 1
+            #LRU 알고리즘을 사용하기 위해 리스트를 이용합니다. 리스트의 맨 앞부분에 있는 요소를 제일 오래 참조가 안된 것(LRU)으로 보고 맨 뒤에 있는 요소를 제일 최근에 참조된 것(MRU)으로 생각합니다.
             cache.remove(city)
             cache.append(city)
         else:
+        #만약 캐시가 없는데 캐시가 꽉차있으면 하나 삭제하고 추가
             if len(cache) == cacheSize:
                 cache.popleft()
             cache.append(city)
