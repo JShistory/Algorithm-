9165159675f0f4711a2ab279ce1d79f4d603873b
김재박 <98608723+JShistory@users.noreply.github.com>
2022-09-27 11:37:14 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/README.md
+++ 프로그래머스/lv2/49993. 스킬트리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 스킬트리 - 49993 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49993) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49993#fnref1) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.01 ms
+메모리: 10.1 MB, 시간: 0.01 ms
 
 ### 구분
 
--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
@@ -2,13 +2,17 @@ def solution(skill, skill_trees):
     answer = 0
     for s in skill_trees:
         stack = []
-        a = True
+        #선행스킬이 있는지 없는지 판단
+        can_skill = True
         for i in s:
+            #만약에 skill_trees에 값을 하나하나 탐색하며 그 스킬이 skill에 포함된다면 추가하고
             if i in skill:
                 stack.append(i)
+                #stack의 인덱스와 skill의 인덱스가 다르면 스킬을 못찍는다고 판단하고 a= False, break
                 if stack.index(i) != skill.index(i):
-                    a = False
+                    can_skill = False
                     break
-        if a:
+        #a가 true이면 스킬찍을수있음
+        if can_skill:
             answer += 1        
     return answer
