715a9dbc6cfdfdb62b8f0816fcd4119092c02f64
김재박 <98608723+JShistory@users.noreply.github.com>
2022-10-11 11:34:01 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 54.44 ms, Memory: 17.9 MB -BaekjoonHub

--- 프로그래머스/lv3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
+++ 프로그래머스/lv3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -4,24 +4,25 @@ def solution(gems):
     answer = []
     check_len = len(set(gems))
     dic = {}
-    
     l,r = 0,0
+    #right포인터를 len(gems)-1만큼 증가시켜서 보석을 담고
     while r < len(gems):
         if gems[r] in dic:
             dic[gems[r]] += 1
         else:
             dic[gems[r]] = 1
-        r += 1
-        
+        r+=1
+        #만약 보석의 개수가 충족됐을 때
         if len(dic) == check_len:
-            while l < r:
+            #left포인터로 보석의 크기가 1이상인 것만 하나씩 줄여간다(최소 보석을 원하기떄문에)
+            while l < r :
                 if dic[gems[l]] > 1:
                     dic[gems[l]] -= 1
                     l += 1
                 elif r-l < N:
                     N = r-l
-                    answer = [l+1, r]
-                    break
+                    answer = [l+1,r]
                 else:
                     break
+
     return answer
