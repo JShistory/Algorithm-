c7c78c747f585b2fda08c7a51c763b41fd5325cb
김재박 <98608723+JShistory@users.noreply.github.com>
2022-10-11 16:12:04 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -5,15 +5,17 @@ def solution(jobs):
     heap = []
     while i < len(jobs):
         for j in jobs:
-            if start < j[0] <= now:
+            #시작값과 현재값안에서 작업할 수 있는 디스크를 다 힙에 넣는다
+            if start < j[0]<= now:
                 heapq.heappush(heap, [j[1], j[0]])
-        
-        if len(heap) > 0 :
+        #만약에 작업할 값이 생긴다면 최소힙을 이용해 최소값을 하나 제거함
+        if len(heap) > 0:
             cur = heapq.heappop(heap)
             start = now
             now += cur[0]
-            answer += (now- cur[1])
-            i +=1
+            answer += (now-cur[1])
+            i += 1
+        #작업할 것이 없다면 now를 1 증가시킴
         else:
             now +=1
     return int(answer/len(jobs))
