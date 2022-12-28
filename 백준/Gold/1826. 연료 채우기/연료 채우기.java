146c74bdda2c97cea358028f233bd9400be534c3
김재박 <98608723+JShistory@users.noreply.github.com>
2022-12-28 10:56:09 +0900

    [Gold II] Title: 연료 채우기, Time: 188 ms, Memory: 17276 KB -BaekjoonHub

--- 백준/Gold/1826. 연료 채우기/연료 채우기.java
+++ 백준/Gold/1826. 연료 채우기/연료 채우기.java
@@ -1,60 +1,63 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<Oil> soil = new PriorityQueue<>();
         int N = Integer.parseInt(br.readLine());
-        PriorityQueue<GasStation> stations = new PriorityQueue<>();
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-
-            stations.add(new GasStation(a, b));
+            soil.add(new Oil(a, b));
         }
 
         st = new StringTokenizer(br.readLine());
         int L = Integer.parseInt(st.nextToken());
         int P = Integer.parseInt(st.nextToken());
-
-        PriorityQueue<Integer> fuels = new PriorityQueue<>(Collections.reverseOrder());
         int answer = 0;
 
-        while (P < L) {
-            while (!stations.isEmpty() && stations.peek().a <= P) {
-                fuels.add(stations.poll().b);
-            }
+        PriorityQueue<Integer> fuel = new PriorityQueue<>(Collections.reverseOrder());
 
-            if (fuels.isEmpty()) {
+        while (P < L) {
+            while (!soil.isEmpty() && soil.peek().distance <= P) {
+                fuel.add(soil.poll().oil_amount);
+            }
+            if (fuel.isEmpty()) {
                 System.out.println(-1);
                 return;
             }
-
             answer++;
-            P += fuels.poll();
-        }
+            P += fuel.poll();
 
+        }
         System.out.println(answer);
+
+
     }
 
-    public static class GasStation implements Comparable<GasStation> {
-        int a;
-        int b;
+    public static class Oil implements Comparable<Oil> {
+        int distance;
+        int oil_amount;
 
-        public GasStation(int a, int b) {
-            this.a = a;
-            this.b = b;
+        public Oil(int distance, int oil_amount) {
+            this.distance = distance;
+            this.oil_amount = oil_amount;
         }
 
         @Override
-        public int compareTo(GasStation o) {
-            return this.a - o.a;
+        public int compareTo(Oil o) {
+            return this.distance - o.distance;
         }
     }
 }
