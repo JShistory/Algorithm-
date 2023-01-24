ea48f704a9c58858503f8d22076aeeb682af368a
김재박 <98608723+JShistory@users.noreply.github.com>
2023-01-24 15:59:45 +0900

    [unrated] Title: 마법의 엘리베이터, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.py
@@ -0,0 +1,19 @@
+def solution(storey):
+    answer = 0
+    
+    while(storey != 0):
+        num = storey % 10
+        if num > 5:
+            answer += (10 - num)
+            storey += 10
+        elif num < 5:
+            answer += (num)
+        else:
+            if (storey // 10) % 10 > 4:
+                storey += 10
+            answer += num
+        storey //= 10
+        print(storey)
+        
+    print(answer)
+    return answer
