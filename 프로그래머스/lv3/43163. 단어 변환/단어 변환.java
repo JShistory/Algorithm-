f40b86a7497dd932b4552a6042c73a32ad17edf3
김재박 <98608723+JShistory@users.noreply.github.com>
2023-05-12 16:51:15 +0900

    [level 3] Title: 단어 변환, Time: 0.02 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.java
@@ -9,8 +9,12 @@ class dataPoint{
     }
 }
 class Solution {
+    static boolean[] visited;
+    static int answer;
     public int solution(String begin, String target, String[] words) {
-        int answer = 0;
+        answer = 0;
+        int count = 0;
+        
         for(String s : words){
             if(s.equals(target)){
                 answer ++;
@@ -19,29 +23,30 @@ class Solution {
         if(answer != 1){
             return 0;
         }
-        answer = 0;
-        Deque<dataPoint> data = new LinkedList<>();
-        data.add(new dataPoint(begin,0));
-        
-        while(!data.isEmpty()){
-            dataPoint temp = data.poll();
-            String checkStr = temp.str;
-            int checkCount = temp.count;
-            if(checkStr.equals(target)){
-                return checkCount;
-            }            
-            for(String s : words){
-                int cnt = 0;
-                for(int i=0; i<s.length(); i++){
-                    if(checkStr.charAt(i) != s.charAt(i)){
-                        cnt++;
-                    }
-                }
-                if(cnt == 1){
-                    data.offer(new dataPoint(s,checkCount+1));
+        visited = new boolean[words.length];
+        dfs(begin,target,words,count);
+        return answer;
+    }
+    public void dfs(String begin, String target, String[] words, int count){
+        if(begin.equals(target)){
+            answer = count;
+            return;
+        }
+        for(int k = 0; k<words.length; k++){
+            int cnt = 0;
+            if(visited[k] == true){
+                continue;
+            }
+            for(int i=0; i<words[k].length(); i++){
+                if(begin.charAt(i) != words[k].charAt(i)){
+                    cnt++;
                 }
             }
+            if(cnt == 1){
+                visited[k] = true;
+                dfs(words[k],target,words,count+1);
+                visited[k] = false;
+            }
         }
-        return answer;
     }
 }
