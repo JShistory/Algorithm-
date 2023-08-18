782facf3c9417a69858609b7e046b612c5af90b5
김재박 <98608723+JShistory@users.noreply.github.com>
2023-08-19 00:25:14 +0900

    [unrated] Title: 숫자 변환하기, Time: 24.74 ms, Memory: 90.5 MB -BaekjoonHub

--- 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.java
@@ -3,9 +3,10 @@ class Solution {
     public int solution(int x, int y, int n) {
         int answer = 0;
         int[] dp = new int[y+1];
-        Arrays.fill(dp, y + 1);
+        Arrays.fill(dp, y+1);
         dp[x] = 0;
         for(int i=x; i<=y; i++){
+        
             if(i+n <= y){
                 dp[i+n] = Math.min(dp[i+n],dp[i]+1);
             }
@@ -15,6 +16,7 @@ class Solution {
             if(i*3 <= y){
                 dp[i*3] = Math.min(dp[i*3],dp[i]+1);
             }
+            
         }
         
         return dp[y] == y+1 ? -1 : dp[y];
