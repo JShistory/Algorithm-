70e99dffdcc895008b4ee3def25efdd7c9525ac4
김재성 <98608723+JShistory@users.noreply.github.com>
2024-10-12 23:42:03 +0900

    [level 2] Title: 카펫, Time: 11.29 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,18 +1,13 @@
 import java.util.*;
 class Solution {
-    private int brown;
-    private int yellow;
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        this.brown = brown;
-        this.yellow = yellow;
-        
         int sumOfCarfet = brown + yellow;
         
         for(int i=3; i<sumOfCarfet; i++) {
-            int j = sumOfCarfet / i;
+            int j =sumOfCarfet / i;
             
-            if (sumOfCarfet % i == 0 && j >= 3){
+            if (sumOfCarfet % i == 0 && j >= 3) {
                 int col = Math.max(i,j);
                 int row = Math.min(i,j);
                 int center = (col - 2) * (row - 2);
