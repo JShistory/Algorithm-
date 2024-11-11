65f5d615ca7c54da172befb5af3791076cc06d40
김재성 <98608723+JShistory@users.noreply.github.com>
2024-11-11 22:25:00 +0900

    [Silver V] Title: 단어 정렬, Time: 384 ms, Memory: 28500 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,34 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.valueOf(br.readLine());
-        List<String> data = new ArrayList<>();
-        for(int i=0; i<N; i++){
-            String input = br.readLine();
-            if(data.contains(input)){
-                continue;
-            }
-            data.add(input);
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        String[] data = new String[N];
+        for(int i=0; i<N; i++) {
+            data[i] = br.readLine();
         }
-        data.sort(new Comparator<String>() {
+        Set<String> set = new HashSet<>(List.of(data));
+        data = set.toArray(new String[0]);
+        Arrays.sort(data, new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
-                if(o1.length() == o2.length()){
+                if (o1.length() == o2.length()) {
                     return o1.compareTo(o2);
                 }
                 return o1.length() - o2.length();
             }
         });
 
-        for(String str : data){
-            System.out.println(str);
+        for(String str : data) {
+            sb.append(str + "\n");
         }
+
+        System.out.println(sb);
     }
 }
